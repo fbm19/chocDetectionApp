697dbfa9d73c464ed0c6a1c94b25a3b0d89cb7f6
fbm <mousha19@yahoo.fr>
2016-12-08 01:11:14 +0100

    second commit

--- app/src/main/java/com/fbm/chocdetectionapp/MainActivity.java
+++ app/src/main/java/com/fbm/chocdetectionapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.fbm.chocdetectionapp;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.gc.materialdesign.views.Switch;
 
@@ -9,7 +13,7 @@ import com.gc.materialdesign.views.Switch;
 public class MainActivity extends AppCompatActivity {
     SensorTask sensorTask;
     Switch switchButton;
-
+    Button butEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -32,7 +36,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+      MyDBHandler db=new MyDBHandler(this);
+        Receiver r=new Receiver(1,"124455","hello");
+        db.addReceiver(r);
+        Receiver r2=new Receiver(1,"000000000","hello");
+        db.replace(r2);
+        EditText txtPhone= (EditText) findViewById(R.id.txtPhone);
+        txtPhone.setText(db.findReceiver().get_number());
+        butEdit= (Button) findViewById(R.id.butEdit);
+        butEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/fbm/chocdetectionapp/Receiver.java
@@ -0,0 +1,50 @@
+package com.fbm.chocdetectionapp;
+
+/**
+ * Created by tahta on 03/05/2016.
+ */
+public class Receiver {
+    private int _id;
+    private String _number;
+    private String _sms;
+
+    public Receiver() {
+
+    }
+
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String get_number() {
+        return _number;
+    }
+
+    public void set_number(String _number) {
+        this._number = _number;
+    }
+
+    public String get_sms() {
+        return _sms;
+    }
+
+    public void set_sms(String _sms) {
+        this._sms = _sms;
+    }
+
+    public Receiver(int _id, String _number, String _sms) {
+        this._id = _id;
+        this._number = _number;
+        this._sms = _sms;
+    }
+
+    public Receiver(String _number, String _sms) {
+        this._number = _number;
+        this._sms = _sms;
+    }
+}
